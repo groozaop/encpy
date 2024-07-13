0ee0c614f9e3134e00ffe48f684ccf234e2e9554
groozaop <140054000+groozaop@users.noreply.github.com>
2024-07-13 17:21:25 +0530

    Create ApxlSTBM3u7Pro.sh

---
+++ ApxlSTBM3u7Pro.sh
@@ -0,0 +1,8 @@
+wget https://github.com/groozaop/te/raw/main/A_pxllSTB%E2%81%B6%E1%B5%9BM3U7PRO2027_NoMadFix.py
+wget https://github.com/groozaop/te/raw/main/pymodul.py
+python3 pymodul.py
+mkdir ./sdcard/
+mkdir ./sdcard/Combo/
+mkdir ./sdcard/ALBSTB/
+git clone https://github.com/DarshGoswami1982/combos.git
+mv ./combos/* ./sdcard/Combo/
